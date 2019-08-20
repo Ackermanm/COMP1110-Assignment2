fb2a5742c1393016ce5c6ffbfe9b0d0d06082bfa
Yafei Liu <u6605935@anu.edu.au>
2019-08-20 13:06:01 +1000

    Bob step7

--- src/gittest/B.java
+++ src/gittest/B.java
@@ -3,7 +3,7 @@ package gittest;
 public class B {
     public static void main(String[] args) {
         System.out.println("A");
-        System.out.println("C");
+        System.out.println("5");
         System.out.println("B");
     }
     @Override
