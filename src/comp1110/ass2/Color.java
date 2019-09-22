ff977fb5321277abf8bca9b80b043ee3f31996d5
tanbowen <u6822656@anu.edu.au>
2019-09-22 19:50:03 +1000

    u6822656

--- src/comp1110/ass2/Color.java
+++ src/comp1110/ass2/Color.java
@@ -1,7 +1,6 @@
 package comp1110.ass2;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public enum Color {
--- src/comp1110/ass2/Rotation.java
+++ src/comp1110/ass2/Rotation.java
@@ -1,15 +1,31 @@
 package comp1110.ass2;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public enum Rotation {
     ZERO, ONE, TWO, THREE;
     public String rotationToNumber(){
         switch (this){
-            case ZERO:return "0";
-            case ONE:return "1";
-            case TWO:return "2";
-            case THREE:return "3";
+            case ZERO:
+                return "0";
+            case ONE:
+                return "1";
+            case TWO:
+                return "2";
+            case THREE:
+                return "3";
         }
         return "0";
     }
-}
 
+
+    @Test
+    public void testRotation() {
+        assertEquals("0", ZERO);
+        assertEquals("1", ONE);
+        assertEquals("2", TWO);
+        assertEquals("3", THREE);
+    }
+}
