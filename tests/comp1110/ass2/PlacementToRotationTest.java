08871c89ecd7f9051732935bb47ed171246c279c
u6822656 <u6822656@anu.edu.au>
2019-09-23 19:38:03 +1000

    tests

--- tests/comp1110/ass2/PlacementToRotationTest.java
+++ tests/comp1110/ass2/PlacementToRotationTest.java
@@ -17,26 +17,74 @@ public class PlacementToRotationTest {
     }
 
     @Test
-    public void PlacementToRotationTest() {
+    public void test_a() {
         test("a000", Rotation.ZERO);
         test("a001", Rotation.ONE);
         test("a002", Rotation.TWO);
         test("a003", Rotation.THREE);
-
-        test("b000", Rotation.ZERO);
-        test("b001", Rotation.ONE);
-        test("b002", Rotation.TWO);
-        test("b003", Rotation.THREE);
-
-        test("c000", Rotation.ZERO);
-        test("c001", Rotation.ONE);
-        test("c002", Rotation.TWO);
-        test("c003", Rotation.THREE);
-
-        test("d000", Rotation.ZERO);
-        test("d001", Rotation.ONE);
-        test("d002", Rotation.TWO);
-        test("d003", Rotation.THREE);
+    }
+    @Test
+    public void test_b() {
+        test("b120", Rotation.ZERO);
+        test("b231", Rotation.ONE);
+        test("b102", Rotation.TWO);
+        test("b223", Rotation.THREE);
+    }
+    @Test
+    public void test_c() {
+        test("c110", Rotation.ZERO);
+        test("c211", Rotation.ONE);
+        test("c312", Rotation.TWO);
+        test("c103", Rotation.THREE);
+    }
+    @Test
+    public void test_d() {
+        test("d220", Rotation.ZERO);
+        test("d131", Rotation.ONE);
+        test("d302", Rotation.TWO);
+        test("d023", Rotation.THREE);
+    }
+    @Test
+    public void test_e() {
+        test("e100", Rotation.ZERO);
+        test("e001", Rotation.ONE);
+        test("e032", Rotation.TWO);
+        test("e213", Rotation.THREE);
+    }
+    @Test
+    public void test_f() {
+        test("f020", Rotation.ZERO);
+        test("f101", Rotation.ONE);
+        test("f332", Rotation.TWO);
+        test("f213", Rotation.THREE);
+    }
+    @Test
+    public void test_g() {
+        test("g300", Rotation.ZERO);
+        test("g101", Rotation.ONE);
+        test("g312", Rotation.TWO);
+        test("g103", Rotation.THREE);
+    }
+    @Test
+    public void test_h() {
+        test("h210", Rotation.ZERO);
+        test("h201", Rotation.ONE);
+        test("h132", Rotation.TWO);
+        test("h203", Rotation.THREE);
+    }
+    @Test
+    public void test_i() {
+        test("i110", Rotation.ZERO);
+        test("i211", Rotation.ONE);
+        test("i332", Rotation.TWO);
+        test("i033", Rotation.THREE);
+    }
+    @Test
+    public void test_j() {
+        test("j210", Rotation.ZERO);
+        test("j201", Rotation.ONE);
+        test("j312", Rotation.TWO);
+        test("j213", Rotation.THREE);
     }
 
 
