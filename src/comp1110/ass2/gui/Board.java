eaa924ba7cb9f96f50641953b5c3f36b1aaff7c7
Yafei Liu <u6605935@anu.edu.au>
2019-10-12 21:18:58 +1100

    Finish task7 and task8

--- src/comp1110/ass2/gui/Board.java
+++ src/comp1110/ass2/gui/Board.java
@@ -1,5 +1,6 @@
 package comp1110.ass2.gui;
-
+import static comp1110.ass2.gui.Level.*;
+import comp1110.ass2.FocusGame;
 import comp1110.ass2.PieceType;
 import comp1110.ass2.Rotation;
 import javafx.application.Application;
@@ -36,14 +37,24 @@ public class Board extends Application {
     private final Group root = new Group();
     private final Group board = new Group();
     private final Group pieces = new Group();
-
+    private final Group challenge = new Group();
 
 
     private static final int ROTATION_THRESHOLD = 50;
-    char[] pieceState = new char[6];   //  all off screen to begin with
 
+    String[] placement= new String[10];
+    int[] pieceState = new int[10];
 
+    String solution = "a000b013c113d302e323f400g420h522i613j701";
+    String challengeString = "RRRBWBBRB";
+    Level lev = EXPERT;
 
+    public Board() {
+        for (int i = 0; i < placement.length; i++){
+            placement[i] = "";
+            pieceState[i] = 0;
+        }
+    }
 
     public Rotation charToRotation(char rotation){
         switch (rotation){
@@ -72,28 +83,8 @@ public class Board extends Application {
     }
 
     class ShowPiece extends ImageView {
-        Image[] images = new Image[4];
         ShowPiece(char piece){
-            /*if (piece < 'a' || piece > 'j') {
-                throw new IllegalArgumentException("Invalid piece: " + piece);
-            }
 
-            for (int rotation = 0; rotation < 4; rotation++){
-                char rot = (char)(rotation+'0');
-                int height = pieceToPieceType(piece).getLengthAndHeight(charToRotation(rot)).getX();
-                int width = pieceToPieceType(piece).getLengthAndHeight(charToRotation(rot)).getY();
-                if (rotation == 1 || rotation == 3){
-                    setFitHeight(width * SQUARE_SIZE);
-                    setFitWidth(height * SQUARE_SIZE);
-                }
-                else {
-                    setFitHeight(height * SQUARE_SIZE);
-                    setFitWidth(width * SQUARE_SIZE);
-                }
-
-                setRotate((rotation)*90);
-                images[rotation] = new Image(Viewer.class.getResource(URI_BASE + piece + ".png").toString());
-            }*/
         }
         ShowPiece(char piece, char rotation) {
             if (piece < 'a' || piece > 'j') {
@@ -114,6 +105,12 @@ public class Board extends Application {
             this.setImage(new Image(Viewer.class.getResource(URI_BASE + piece + ".png").toString()));
 
             this.setEffect(dropShadow);
+        }
+        ShowPiece(String color) {
+            setFitHeight(SQUARE_SIZE);
+            setFitWidth(SQUARE_SIZE);
+            this.setImage(new Image(Viewer.class.getResource(URI_BASE + "sq-" + color.toLowerCase() + ".png").toString()));
+            setEffect(dropShadow);
         }
     }
 
@@ -187,74 +184,209 @@ public class Board extends Application {
         }
 
         public void snapToGrid() {
-//            if (getLayoutX() < PLAY_AREA_X || getLayoutY() < PLAY_AREA_Y ||
-//                    getLayoutX() > PLAY_AREA_X + (9*SQUARE_SIZE)|| getLayoutY()>PLAY_AREA_Y + (5*SQUARE_SIZE)){
-//                snapToOrigin();
-//            }
-//            else {
-                double[] distance = new double[45];
-                double min = 100;
-                int k = 0;
-                int xy = 0;
-                int xLocation;
-                int yLocation;
-
-                double x = (getLayoutX() - PLAY_AREA_X) / SQUARE_SIZE;
-                double y = (getLayoutY() - PLAY_AREA_Y) / SQUARE_SIZE;
-                for (int i = 0; i < 5; i++) {
-                    for (int j = 0; j < 9; j++) {
-                        distance[k] = (((x - j) * (x - j)) + ((y - i) * (y - i)));
-                        min = Math.min(distance[k], min);
-                        k++;
+            if (onBoard()) {
+                int[] layout = transformLayout();
+                int xLocation = layout[0];
+                int yLocation = layout[1];
+                if (boardIsPlacementValid(layout)) {
+                    if (rotation == 1 || rotation == 3) {
+                        if (piece == 'a' || piece == 'd' || piece == 'e' || piece == 'g') {
+                            setLayoutX(PLAY_AREA_X + xLocation * SQUARE_SIZE - 0.5 * SQUARE_SIZE);
+                            setLayoutY(PLAY_AREA_Y + yLocation * SQUARE_SIZE + 0.5 * SQUARE_SIZE);
+                        }
+                        if (piece == 'b' || piece == 'c' || piece == 'j' || piece == 'f') {
+                            setLayoutX(PLAY_AREA_X + xLocation * SQUARE_SIZE - SQUARE_SIZE);
+                            setLayoutY(PLAY_AREA_Y + yLocation * SQUARE_SIZE + SQUARE_SIZE);
+                        }
+                    } else {
+                        setLayoutX(PLAY_AREA_X + xLocation * SQUARE_SIZE);
+                        setLayoutY(PLAY_AREA_Y + yLocation * SQUARE_SIZE);
                     }
                 }
-                for (int i = 0; i < 45; i++) {
-                    if (min == distance[i]) {
-                        xy = i;
+                else snapToOrigin();
+            }
+            else {
+                snapToOrigin();
+            }
+            checkCompletion();
+        }
+        public void initialization(String placement){
+            int[] layout = new int[2];
+            layout[0] = placement.charAt(1) - '0';
+            layout[1] = placement.charAt(2) - '0';
+            int xLocation = layout[0];
+            int yLocation = layout[1];
+            rotation = placement.charAt(3) - '0';
+                if (boardIsPlacementValid(layout)) {
+                    if (rotation == 1 || rotation == 3) {
+                        if (piece == 'a' || piece == 'd' || piece == 'e' || piece == 'g') {
+                            setLayoutX(PLAY_AREA_X + xLocation * SQUARE_SIZE - 0.5 * SQUARE_SIZE);
+                            setLayoutY(PLAY_AREA_Y + yLocation * SQUARE_SIZE + 0.5 * SQUARE_SIZE);
+                        }
+                        if (piece == 'b' || piece == 'c' || piece == 'j' || piece == 'f') {
+                            setLayoutX(PLAY_AREA_X + xLocation * SQUARE_SIZE - SQUARE_SIZE);
+                            setLayoutY(PLAY_AREA_Y + yLocation * SQUARE_SIZE + SQUARE_SIZE);
+                            double a = getLayoutX();
+                            double b = getLayoutY();
+                        }
+                    } else {
+                        setLayoutX(PLAY_AREA_X + xLocation * SQUARE_SIZE);
+                        setLayoutY(PLAY_AREA_Y + yLocation * SQUARE_SIZE);
+                    }
+                }
+                else snapToOrigin();
+            setRotate(rotation * 90);
+        }
+        public int[] transformLayout(){
+            double[][] distance = new double[5][9];
+            int[] layout = new int[2];
+            double min = 100000;
+            int rowLocation = -1;
+            int columnLocation = -1;
+            double x = 0;
+            double y = 0;
+            if (rotation == 1 || rotation == 3) {
+                if (piece == 'a' || piece == 'd' || piece == 'e' || piece == 'g') {
+                    x = (getLayoutX() + 0.5 * SQUARE_SIZE - PLAY_AREA_X) / SQUARE_SIZE;
+                    y = (getLayoutY() - 0.5 * SQUARE_SIZE - PLAY_AREA_Y) / SQUARE_SIZE;
+                }
+
+                if (piece == 'b' || piece == 'c' || piece == 'j' || piece == 'f') {
+                    x = (getLayoutX() + SQUARE_SIZE - PLAY_AREA_X) / SQUARE_SIZE;
+                    y = (getLayoutY() - SQUARE_SIZE - PLAY_AREA_Y) / SQUARE_SIZE;
+                }
+            } else {
+                x = (getLayoutX() - PLAY_AREA_X) / SQUARE_SIZE;
+                y = (getLayoutY() - PLAY_AREA_Y) / SQUARE_SIZE;
+            }
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 9; j++) {
+                    distance[i][j] = (((x - j) * (x - j)) + ((y - i) * (y - i)));
+                    min = Math.min(distance[i][j], min);
+                }
+            }
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 9; j++) {
+                    if (min == distance[i][j]) {
+                        columnLocation = j;
+                        rowLocation = i;
                         break;
                     }
                 }
-                xLocation = xy % 9;
-                yLocation = xy / 9;
-
-                setLayoutX(PLAY_AREA_X + xLocation * SQUARE_SIZE);
-                setLayoutY(PLAY_AREA_Y + yLocation * SQUARE_SIZE);
-//            }
+            }
+            layout[0] = columnLocation;
+            layout[1] = rowLocation;
+            return layout;
         }
-        public void snapToOrigin(){
+        private void snapToOrigin(){
+            rotation = -1;
+            rotate();
             setLayoutX(originX);
             setLayoutY(originY);
+            pieceState[piece-'a'] = 0;
+            placement[piece-'a'] = "";
             }
         private void rotate() {
             rotation = (rotation + 1) % 4;
             setRotate(rotation * 90);
-            char rot = (char)(rotation+'0');
-            int height = pieceToPieceType(piece).getLengthAndHeight(charToRotation(rot)).getX();
-            int width = pieceToPieceType(piece).getLengthAndHeight(charToRotation(rot)).getY();
-            if (rotation == 0 || rotation == 2){
-                setFitHeight(height * SQUARE_SIZE);
-                setFitWidth(width * SQUARE_SIZE);
-            }
             if (rotation == 1 || rotation == 3) {
-                setFitHeight(width * SQUARE_SIZE);
-                setFitWidth(height * SQUARE_SIZE);
-            }
-            setImage(new Image(Viewer.class.getResource(URI_BASE + piece + ".png").toString()));
-            toFront();
-//            setPosition();
-        }
-    }
+                if (piece == 'a' || piece == 'd' || piece == 'e' || piece == 'g') {
+                    setLayoutX(getLayoutX() - 0.5 * SQUARE_SIZE);
+                    setLayoutY(getLayoutY() + 0.5 * SQUARE_SIZE);
+                }
 
-    private void makePieces() {
-        pieces.getChildren().clear();
-        for (char i = 'a'; i <= 'j'; i++) {
-            pieces.getChildren().add(new DraggablePiece(i));
+                if (piece == 'b' || piece == 'c' || piece == 'j' ||  piece == 'f') {
+                    setLayoutX(getLayoutX() - SQUARE_SIZE);
+                    setLayoutY(getLayoutY() + SQUARE_SIZE);
+                }
+            }
+            else if (rotation == 0 || rotation == 2){
+                if (piece == 'a' || piece == 'd' || piece == 'e' || piece == 'g') {
+                    setLayoutX(getLayoutX() + 0.5 * SQUARE_SIZE);
+                    setLayoutY(getLayoutY() - 0.5 * SQUARE_SIZE);
+                }
+
+                if (piece == 'b' || piece == 'c' || piece == 'j' ||  piece == 'f') {
+                    setLayoutX(getLayoutX() + SQUARE_SIZE);
+                    setLayoutY(getLayoutY() - SQUARE_SIZE);
+                }
+            }
+            toFront();
+//            int[] layout = transformLayout();  //  xuanzhuan bu tongguo
+//            if (!boardIsPlacementValid(layout)) snapToOrigin();
+        }
+
+        public boolean onBoard(){
+            if (getLayoutX() < PLAY_AREA_X - SQUARE_SIZE) return false;
+            if (getLayoutY() < PLAY_AREA_Y - SQUARE_SIZE) return false;
+            if (getLayoutX() > BOARD_X + BOARD_WIDTH + SQUARE_SIZE) return false;
+            if (getLayoutY() > BOARD_Y + BOARD_HEIGHT + SQUARE_SIZE) return false;
+            return true;
+        }
+        public boolean boardIsPlacementValid(int[] layout){
+            if (pieceState[piece-'a'] == 1){
+                pieceState[piece-'a'] = 0;
+                placement[piece-'a'] = "";
+            }
+            String allPlacement = "";
+            String pieceString = piece + "" + layout[0] + "" + layout[1] + "" + rotation;
+
+            for (int i = 0; i < placement.length; i++){
+                allPlacement += placement[i];
+            }
+            allPlacement += pieceString;
+            if (FocusGame.isPlacementStringValid(allPlacement)){
+                pieceState[piece-'a'] = 1;
+                placement[piece-'a'] = pieceString;
+                return true;
+            }
+            else return false;
+        }
+        public boolean checkCompletion(){
+            for (int i = 0; i < pieceState.length; i++){
+                if (pieceState[i] != 1)return false;
+            }
+            return true;
         }
     }
+//    private void makePieces() {
+//        pieces.getChildren().clear();
+//        for (char i = 'a'; i <= 'j'; i++) {
+//            pieces.getChildren().add(new DraggablePiece(i));
+//        }
+//    }
 
 
     // FIXME Task 8: Implement challenges (you may use challenges and assets provided for you in comp1110.ass2.gui.assets: sq-b.png, sq-g.png, sq-r.png & sq-w.png)
 
+    public void challenges(String solution, String challenge, Level level){
+        this.challenge.getChildren().clear();
+        this.pieces.getChildren().clear();
+        this.challenge.getChildren().clear();
+        ShowPiece[] show = new ShowPiece[challenge.length()];
+        DraggablePiece[] draggablePieces = new DraggablePiece[10];
+        for (int i = 0; i < challenge.length(); i++){
+            show[i] = new ShowPiece(challenge.substring(i,i+1));
+        }
+        for (int i = 0; i < challenge.length(); i++){
+            int x = i % 3;
+            int y = i / 3;
+            show[i].setLayoutX(PLAY_AREA_X + 3 * SQUARE_SIZE + x * SQUARE_SIZE);
+            show[i].setLayoutY(BOARD_Y + BOARD_HEIGHT + SQUARE_SIZE + y * SQUARE_SIZE);
+        }
+        for (int i = 0; i < challenge.length(); i++) {
+            this.challenge.getChildren().add(show[i]);
+        }
+        for (char i = 'a'; i <= 'j'; i++) {
+            draggablePieces[i-'a'] = new DraggablePiece(i);
+            this.pieces.getChildren().add(draggablePieces[i-'a']);
+        }
+        for (int i = 0; i < 3-level.levelToInt(level); i++){
+            String placement = solution.substring(i*4,i*4+4);
+            draggablePieces[i].initialization(placement);
+        }
+
+    }
     // FIXME Task 10: Implement hints
 
     // FIXME Task 11: Generate interesting challenges (each challenge may have just one solution)
@@ -266,8 +398,9 @@ public class Board extends Application {
         Scene scene = new Scene(root, VIEWER_WIDTH, VIEWER_HEIGHT);
         root.getChildren().add(pieces);
         root.getChildren().add(board);
-
-        makePieces();
+        root.getChildren().add(challenge);
+        challenges(solution, challengeString, lev);
+//        makePieces();
         makeBoard();
         primaryStage.setScene(scene);
         primaryStage.show();
