ac342a859f6560a3ee01962b90c22221358f46bf
Yafei Liu <u6605935@anu.edu.au>
2019-10-16 21:51:22 +1100

    Different challenges

---
+++ src/comp1110/ass2/gui/Solution.java
@@ -0,0 +1,11 @@
+package comp1110.ass2.gui;
+
+public class Solution {
+    String objective;
+    String placement;
+
+    public Solution(String iObjective, String iPlacement) {
+        objective = iObjective;
+        placement = iPlacement;
+    }
+}
